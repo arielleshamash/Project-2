eb59e598d0e06f2180b7cce3f9aa7e7e3a0184ce
Camerone Stoney <Cameronestoney@yahoo.com>
2019-07-27 13:45:16 -0700

    sudo code added to apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,40 +6,22 @@ module.exports = function(app) {
 
 //remove event
 app.post("/api/")
-//add data for event
-//edit event to cross it off 
+
+// add event, cross off, delete
+
+// create fake post to know what is coming
 
 
-
-
-  // Get all examples
-<<<<<<< HEAD
-  app.get("/api/edit-todo", function(req, res) {
-    console.log(req.body);
-    var newEvent = req.body;
-    planitDB
-=======
-  app.get("/", function(req, res) {
-
-    let currentDate = moment().format("MM")
-
-    db.Holiday.findAll({where:{
-      date: currentDate 
-    }}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
-    
->>>>>>> 23a82371a3751597a24dbd245e59cd34db77474c
-  });
-
-  // Create a new example
+  // Create a new event
   app.post("/api/new-todo", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
+    db.Event.create(req.body).then(function(dbExample) {
       res.json(dbExample);
     });
   });
 
-  // Delete an example by id
+  // crossout an item on the todo list
+
+  // Delete an event
   app.delete("/api/rem-todo/:id", function(req, res) {
     db.event.destroy({ where: { id: req.params.id } }).then(function(db) {
       res.json(db);
