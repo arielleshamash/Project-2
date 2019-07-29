84d20ff72d9b8669f2e0f29b6e51acc69f31ed82
Camerone Stoney <Cameronestoney@yahoo.com>
2019-07-28 20:01:42 -0700

    WIP for api routes.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -13,17 +13,45 @@ app.post("/api/")
 
 
   // Create a new event
+var newevent = {
+  eventName = $("#...").val(),
+  eventYear = $("#...").val(),
+  eventMonth = $("#...").val(),
+  eventDay = $("#...").val(),
+  eventFinished = $("#...").val(),
+  eventImportnant = $("#...").val(),
+}
+
   app.post("/api/new-todo", function(req, res) {
-    db.Event.create(req.body).then(function(dbExample) {
+    db.Event.create({
+      event: eventName,
+      year: eventYear,
+      month: eventMonth,
+      day: eventDay,
+      finished: eventFinished,
+      important: eventImportnant
+    })
+    .then(function(dbExample) {
       res.json(dbExample);
     });
   });
 
   // crossout an item on the todo list
+  app.put("/api/update", function(req, res){
+    db.Event.update(req.body.important,
+      {
+        where:{
+          event: req.body.event
+        }
+      }).then(function(dbPost){
+        res.json(dbPost);
+      });
+  });
 
   // Delete an event
-  app.delete("/api/rem-todo/:id", function(req, res) {
-    db.event.destroy({ where: { id: req.params.id } }).then(function(db) {
+  app.delete("/api/delete-event", function(req, res) {
+    const id = req.params.id
+    db.Event.destroy({ where: { id: id } }).then(function(db) {
       res.json(db);
     });
   });
