bfd4bad1a7aae064ad1f2161790574360bd8fbed
gccunanan <gccunanan@gmail.com>
2019-07-30 03:45:31 -0700

    reverting files back

--- routes/events.js
+++ routes/events.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const Event = require('../models/Event.js')
 
 // creating some seeds for the database
 router.get('/', (req, res)=>res.send("it's kinda working"));
