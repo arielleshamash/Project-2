05aaa307d20b444ef13bdda09a279cad65922aa4
gccunanan <gccunanan@gmail.com>
2019-07-30 00:29:56 -0700

    missing columns; added column createdAt & updatedAt with CURRENT_STAMP & ON UPDATE CURRENT_TIMESTAMP

--- models/schema.sql
+++ models/schema.sql
@@ -3,7 +3,7 @@ CREATE DATABASE `cvvjamnkiu5fjxdi`;
 
 USE `cvvjamnkiu5fjxdi`;
 
-CREATE TABLE `event`(
+CREATE TABLE `events` (
     `id` INTEGER AUTO_INCREMENT NOT NULL,
     `moment` VARCHAR(13),
     `event` VARCHAR(30) NOT NULL,
@@ -12,10 +12,12 @@ CREATE TABLE `event`(
     `day` INTEGER NOT NULL,
     `finished` BOOLEAN NOT NULL DEFAULT FALSE,
     `important` BOOLEAN NOT NULL DEFAULT FALSE ,
+    `createdAt` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `updatedaAt` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY(id)
 );
 
-INSERT INTO `event`(`event`,`year`,`month`, `day`) 
+INSERT INTO `events` (`event`,`year`,`month`, `day`) 
 VALUES("Independence Day", 2019, 07, 04), ("Division/Clinton Street Fair", 2019, 07, 27),("Adult Party at Omsi", 2019, 07, 31),("Cold Coffe Festival", 2019, 08, 03),
 ("Alberta Street Fair", 2019, 08, 10), ("Bike Or Walk The Bridges", 2019, 08, 23), ("Bike Or Walk The Bridges", 2019, 08, 24), ("Bike Or Walk The Bridges", 2019, 08, 25),
 ("Camera Show", 2019, 08, 24), ("Air and vehicle show", 2019, 08, 17), ("Brunchy booze festival", 2019, 08, 10), ("Hawaiian festival", 2019, 07, 27), ("Hawaiian festival", 2019, 07 ,28);
