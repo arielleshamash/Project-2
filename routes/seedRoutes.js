dfcc26a9f801624c8a14d87432c252707a74beb0
gccunanan <gccunanan@gmail.com>
2019-07-30 03:06:45 -0700

    created an "Event" model to call upon to export to other files

--- routes/seedRoutes.js
+++ routes/seedRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const Event = require('../models/Event.js')
 
 // creating some seeds for the database
 router.get('/', (req, res)=>res.send("it's kinda working"));
