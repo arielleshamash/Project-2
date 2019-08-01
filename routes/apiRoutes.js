8a3ff361dfc5389f88e6b9db7f66dfa672038460
gccunanan <gccunanan@gmail.com>
2019-07-31 17:59:34 -0700

    updating files to most current version

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -21,19 +21,6 @@ module.exports = function (app) {
       });
   });
 
-<<<<<<< HEAD
-  app.post("/api/new-todo", function(req, res) {
-    db.event.create({
-      event: req.eventName,
-      year: req.eventYear,
-      month: req.eventMonth,
-      day: req.eventDay,
-      finished: req.eventFinished,
-      important: req.eventImportnant
-    })
-    .then(function(dbExample) {
-      res.json(dbExample);
-=======
     //get data by exact event
     app.get("/event/:id", function (req, res) {
       var id = req.params[0];
@@ -42,46 +29,19 @@ module.exports = function (app) {
           listItem: listing
         })
       })
->>>>>>> 6b753fd6ca1c4f2e5ec6db2f2f3a2ed5a5ef7e12
     });
 
-<<<<<<< HEAD
-  // still needs work!!!!!
-  app.get("api/todo", function(req, res){
-    db.event.selectAll({where:{
-      day: 30
-    }})
-  })
-
-  // crossout an item on the todo list
-  app.put("/api/update", function(req, res){
-    db.event.update(req.body.important,
-      {
-        where:{
-          event: req.body.event
-=======
 
     //get important data
     app.get("/important-list/:month", function (req, res) {
       db.Event.findAll({
         where: {
           important: true
->>>>>>> 6b753fd6ca1c4f2e5ec6db2f2f3a2ed5a5ef7e12
         }
       }).then(function (veryImportant) {
         res.render("home", {
           importantList: veryImportant
         });
       });
-<<<<<<< HEAD
-  });
-
-  // Delete an event
-  app.delete("/api/delete-event", function(req, res) {
-    const id = req.params.id
-    db.event.destroy({ where: { id: id } }).then(function(db) {
-      res.json(db);
-=======
->>>>>>> 6b753fd6ca1c4f2e5ec6db2f2f3a2ed5a5ef7e12
     });
   };
