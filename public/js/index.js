9be984e68fa2efaa375885a7eeb3ebd66637bdba
Camerone Stoney <Cameronestoney@yahoo.com>
2019-08-01 18:30:47 -0700

    final push

--- public/js/index.js
+++ public/js/index.js
@@ -1,61 +1,69 @@
-$(document).ready(function(){
+// $(document).ready(function(){
 
-    console.log("running")
+//     console.log("running")
 
-    $("#add").unbind("click").click(function () {
+//     $("#add").unbind("click").click(function () {
 
-        var dateString= $("#date").html()
-        var dateValues = dateString.split(" - ")
+//         var dateString= $("#date").html()
+//         var dateValues = dateString.split(" - ")
         
-        var newEvent = {
-            eventName: $("#input").val(),
-            eventMonth: dateValues[0],
-            eventDay: dateValues[1],
-            eventYear: dateValues[2]
-            // eventFinished: $("#...").val(),
-            // eventImportnant: $("#...").val()
-        }
-        // console.log("this is "+ JSON.stringify(newEvent));
-        $.ajax({
-            method: "POST",
-            url: "/api/new-todo",
-            data: newEvent
-        }).then(function (data) {
-            // console.log(data)
-        });
+//         var newEvent = {
+//             eventName: $("#input").val(),
+//             eventMonth: dateValues[0],
+//             eventDay: dateValues[1],
+//             eventYear: dateValues[2]
+//             // eventFinished: $("#...").val(),
+//             // eventImportnant: $("#...").val()
+//         }
+//         // console.log("this is "+ JSON.stringify(newEvent));
+//         $.ajax({
+//             method: "POST",
+//             url: "/api/new-todo",
+//             data: newEvent
+//         }).then(function (data) {
+//             // console.log(data)
+//         });
         
-    });
+//     });
 
-// delete function
-
-    // $(document).on("click", '.delete', function(){
-    //     console.log(arguments[0])   
-    //     var dateString= $(".itemText").length()
-    //     var dateValues = dateString.split(" - ")
-
-    //     var deleteEvent = {
-    //         eventName: $(`#item${id}`).text(),
-    //         eventMonth: dateValues[0],
-    //         eventDay: dateValues[1],
-    //         eventYear: dateValues[2]
-    //     }
-
-    //     // console.log(deleteEvent);
-
-    //     $.ajax({
-    //         method: "POST",
-    //         url: "/api/delete-event",
-    //         data: deleteEvent
-    //     }).then(function (data) {
-    //         console.log("deleting "+data)
-    //     });
-    // })
+// // delete function
+//     $(document).on("click", '.delete', function(){
+//         var titleElement = document.getElementById('data-id').innerHTML();
+//         console.log(titleElement);
+//         // var titleChild = titleElement.getElementsByTagName('p');
 
 
+//         // console.log(titleChild[0].innerhtml());
+//         var dateString= $(".itemText").val();
+//         var dateValues = dateString.split(" - ")
 
-    // ---------------------------- for completed todos --------------------------------
+//         var deleteEvent = {
+//             // eventName: $(`#item${id}`).text(),
+//             eventMonth: dateValues[0],
+//             eventDay: dateValues[1],
+//             eventYear: dateValues[2]
+//         }
 
-    $(".complete").on("click", function(){
-        $(this).attr("data-id");
-    })
-})
+//         // console.log(deleteEvent);
+
+//         $.ajax({
+//             method: "POST",
+//             url: "/api/delete-event",
+//             data: deleteEvent
+//         }).then(function (data) {
+//             console.log("deleting "+data)
+//         });
+        
+//     })
+    
+    
+    
+    
+    
+//     // ---------------------------- for completed todos --------------------------------
+    
+//     $(".complete").on("click", function(){
+//         $(this).attr("data-id");
+//     })
+//     // console.log("end of script")
+// })
